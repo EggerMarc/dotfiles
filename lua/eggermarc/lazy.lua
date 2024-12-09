9e721deed3168b19a85830729b3c1d5f4a92bac9
EggerMarc <eggermarc@gmail.com>
2024-12-09 11:18:26 -0300

    add: snacks, no config yet

--- lua/eggermarc/lazy.lua
+++ lua/eggermarc/lazy.lua
@@ -152,6 +152,21 @@ local plugins = {
         lazy = false,   -- This plugin is already lazy
     },
     'mfussenegger/nvim-dap',
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
+        },
+    }
 }
 
 
