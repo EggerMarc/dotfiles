059db779bc9963e4f988204124f40672aca59f11
EggerMarc <eggermarc@gmail.com>
2026-04-09 15:08:34 -0300

    fix(nvim/harpoon): rebinded harpoon to <leader>ha to avoid conflict with avante

--- common/nvim/after/plugin/harpoon.lua
+++ common/nvim/after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui= require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>ah", mark.add_file)
+vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu)
 vim.keymap.set("n", "<C-y>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-u>", function() ui.nav_file(2) end)
